1b9c6454cf8b103d3339a94e41da0bff5e94799b
Kubalak <79917041+Kubalak@users.noreply.github.com>
2021-03-31 14:54:40 +0200

    Add files via upload

---
+++ src/user/Account.java
@@ -0,0 +1,18 @@
+package user;
+
+class Account
+{
+    private double credit;
+    public Account(double credit)
+    {
+        this.credit = credit;
+    }
+    public double checkCredit(){return credit;}
+    public boolean changeCredit(double gap)
+    {
+        if(gap < 0.0 && credit + gap < 0.0)return false;
+       credit += gap;
+       return true;
+    }
+
+}
